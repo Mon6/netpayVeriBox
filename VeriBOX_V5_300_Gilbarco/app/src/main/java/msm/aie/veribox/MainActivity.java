9738ff74749b2c44c7d8ac6a243d2850c8fd4491
Mon6 <moises.castrejon.mendez@outlook.com>
2021-11-11 13:02:09 -0600

    Redimencionado en tienda

--- VeriBOX_V5_300_Gilbarco/app/src/main/java/msm/aie/veribox/MainActivity.java
+++ VeriBOX_V5_300_Gilbarco/app/src/main/java/msm/aie/veribox/MainActivity.java
@@ -3693,7 +3693,7 @@ public void kiosco(View view) {
 	}
 
     public void prueba(View view) {
-		setContentView(R.layout.preset);
+		setContentView(R.layout.tienda);
     }
 
 //FUNCIONES DE BLUETOOTH
